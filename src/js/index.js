eed6cd551568e7682c3d5c44d12a650b8e1c8faa
Remi Mathon <mthremi@gmail.com>
2021-03-03 19:28:26 +0100

    Add Info + Etudes

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
-import * as mdb from 'mdb-ui-kit';
+const date1 = new Date('07/31/2000');
+const date2 = new Date();
 
-export default {
-  mdb,
-};
+const Difftemps = date2.getTime() - date1.getTime();
+const Diffjours = Difftemps / (1000 * 3600 * 24);
+document.getElementById('nbDays').innerHTML = String(Math.round(Diffjours));
+
+document.getElementById('nbCafe').innerHTML = String(Math.trunc(Math.random() * (450 - 150)));
