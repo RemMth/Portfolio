b24f9995ece69efbaec2840e77921a6e392cadb4
Remi Mathon <mthremi@gmail.com>
2021-03-10 13:49:53 +0100

    Modif

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const date1 = new Date('07/31/2000');
-const date2 = new Date();
-
-const Difftemps = date2.getTime() - date1.getTime();
-const Diffjours = Difftemps / (1000 * 3600 * 24);
-document.getElementById('nbDays').innerHTML = String(Math.round(Diffjours));
-
-document.getElementById('nbCafe').innerHTML = String(Math.trunc(Math.random() * (450 - 150)));
